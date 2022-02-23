1f82c33295e74dd17cd9c7797ed39e11284f07a4
kareemAmarah95 <kareemamarah2356@gmail.com>
2022-02-23 17:40:28 +0200

    ecommerce_react

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,32 +2,43 @@ import React from 'react'
 import {Card , CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 import useStyles from "./styles";
-const Product = ({product}) => {
+const Product = ({hambozo}) => {
     const classes = useStyles();
+const handleAddToCart = () => onAddToCart(hambozo.id,1);
   return (
     <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
-            <CardContent>
-                <div className={classes.cardContent}>
-                    <Typography variant="h5" gutterBottom>
-                        {product.name}
-                    </Typography>
+      <CardMedia
+        className={classes.media}
+        image={hambozo.media.source}
+        title={hambozo.name}
+      />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom component="h2">
+            {/* {product.price.formatted_with_symbol} */}
+            {hambozo.name}
+          </Typography>
 
-                    <Typography variant="h5">
-                        {product.price}
-                    </Typography>
-                </div>
-                <Typography variant="body2" color="textSecondary">
-                    {product.description}
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Add To Cart">
-                    <AddShoppingCart/>
-                </IconButton>
-            </CardActions>
+          <Typography gutterBottom variant="h5" component="h2">
+            ${hambozo.price.formatted}
+          </Typography>
+        </div>
+        <Typography
+          dangerouslySetInnerHTML={{ __html: hambozo.description }}
+          variant="body2"
+          color="textSecondary"
+          component="p"
+        >
+          {hambozo.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add To Cart" onClick={handleAddToCart}>
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
-  )
+  );
 }
 
 export default Product
